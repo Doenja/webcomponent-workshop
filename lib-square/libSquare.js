6bbe4a157818007e3af6f4d845849c3aead33e2e
Doenja <doenjamaris@gmail.com>
2021-03-19 21:48:32 +0100

    Clear square

--- lib-square/libSquare.js
+++ lib-square/libSquare.js
@@ -1,41 +1,11 @@
 class Square extends HTMLElement {
     constructor() {
         super()
-    
-        // Attatch the shadow dom
-        const shadow = this.attachShadow({mode: 'open'})
-        
-        // Link the stylesheet
-        const linkElem = document.createElement('link');
-        linkElem.setAttribute('rel', 'stylesheet');
-        linkElem.setAttribute('href', 'lib-square/styles.css');
-        shadow.appendChild(linkElem);
+    }
 
-        // Create a wrapper and give it a class
-        const wrapper = document.createElement('div');
-        wrapper.setAttribute('class','wrapper');
-
-        wrapper.style.backgroundColor = "indianred"
-        wrapper.style.width = "100px"
-        wrapper.style.height = "100px"
-
-        // If the size attribute is set, adjust the wrapper style
-        if(this.hasAttribute('size')) {
-            wrapper.style.width = this.getAttribute('size')
-            wrapper.style.height = this.getAttribute('size')
-        }
-
-        // If an image is set, create it and add it to the wrapper
-        if(this.hasAttribute('img')) {
-            const img = document.createElement('img')
-            img.src=this.getAttribute('img')
-            wrapper.appendChild(img)
-        }
-
-        // Add the wrapper to the shadowroot
-        this.shadowRoot.append(wrapper)
+    connectedCallback() {
+        this.innerHTML = `<h1>Hello world</h1>`;
     }
 }
 
-customElements.define('lib-square', Square)
 
